dc23233ea0b6651932dfa318d81bb19d473224fa
willy chang <willy.chang@solmate.cc>
2021-05-11 21:19:51 +0800

    add column

---
+++ db/migrate/20210511131910_user_add_text_type_column.rb
@@ -0,0 +1,5 @@
+class UserAddTextTypeColumn < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :settings, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_083513) do
+ActiveRecord::Schema.define(version: 2021_05_11_131910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_083513) do
     t.string "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "settings"
   end
 
 end
